6239fe546029278a0a999244be534665dd81dbb6
Gil Do <gildoron8@gmail.com>
2021-10-31 17:41:20 +0200

    Add files via upload

---
+++ BookStore/ViewModels/ListViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using BookStore.Models;
+namespace BookStore.ViewModels
+{
+    public class ListViewModels
+    {
+        public string genre { get; set; }
+        public int countBook { get; set; }
+    }
+}
